6c9d671f9408bb1068304d17d900f8b7fb7ea598
AjitSingh <jeetsingh.ajit@gmail.com>
2017-03-22 08:26:32 +0530

    Wrapping sinatra server in Server class

--- lib/pairing_matrix/server/server.rb
+++ lib/pairing_matrix/server/server.rb
@@ -1,15 +1,19 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require_relative '../../pairing_matrix'
 
-config_reader = PairingMatrix::ConfigReader.new('pairing_matrix.yml')
-config = config_reader.config
-commit_reader = PairingMatrix::CommitReader.new(config)
+module PairingMatrix
+  class Server < Sinatra::Base
+    config_reader = PairingMatrix::ConfigReader.new('pairing_matrix.yml')
+    config = config_reader.config
+    commit_reader = PairingMatrix::CommitReader.new(config)
 
-get '/data/:days' do
-  commit_reader.authors_with_commits(params['days'].to_i).to_json
-end
+    get '/data/:days' do
+      commit_reader.authors_with_commits(params['days'].to_i).to_json
+    end
 
-get '/matrix' do
-  File.read(File.join('lib/pairing_matrix/server/public', 'index.html'))
+    get '/matrix' do
+      File.read(File.join('lib/pairing_matrix/server/public', 'index.html'))
+    end
+  end
 end
