8e6a779d32b8ca67478679c8921eadd872e8fa64
Sumit Gupta <sumit.gpt@outlook.com>
2018-03-19 22:28:05 +0530

    Adding support for github enterprise

--- lib/pairing_matrix/config/config.rb
+++ lib/pairing_matrix/config/config.rb
@@ -1,12 +1,13 @@
 module PairingMatrix
   class Config
-    attr_reader :repos, :authors_regex, :github_access_token, :github_repos
+    attr_reader :repos, :authors_regex, :github_access_token, :github_repos, :github_url
 
-    def initialize(repos, authors_regex, github_access_token, github_repos)
+    def initialize(repos, authors_regex, github_access_token, github_repos, github_url)
       @repos = repos
       @authors_regex = authors_regex
       @github_access_token = github_access_token
       @github_repos = github_repos
+      @github_url = github_url
     end
 
     def fetch_from_github?
@@ -15,6 +16,10 @@ module PairingMatrix
 
     def has_github_access_token?
       !@github_access_token.nil? && !@github_access_token.empty?
+    end
+
+    def github_enterprise?
+      !@github_url.nil? && !@github_url.empty?
     end
   end
 end
